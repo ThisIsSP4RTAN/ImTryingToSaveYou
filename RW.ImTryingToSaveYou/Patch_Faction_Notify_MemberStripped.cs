331beb3fe8badd9ee14acb048298113d606d563b
ThisIsSP4RTAN <61633596+ThisIsSP4RTAN@users.noreply.github.com>
2025-07-26 02:41:01 -0500

    Add project files.

---
+++ RW.ImTryingToSaveYou/Patch_Faction_Notify_MemberStripped.cs
@@ -0,0 +1,63 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+using System.Collections.Generic;
+using Verse.AI;
+
+namespace ImTryingToSaveYou
+{
+    [StaticConstructorOnStartup]
+    public static class Patch_Faction_Notify_MemberStripped
+    {
+        // Keep track of which pawns are being forcibly dressed
+        public static readonly HashSet<Pawn> JustForceDressed = new HashSet<Pawn>();
+
+        static Patch_Faction_Notify_MemberStripped()
+        {
+            var harmony = new Harmony("ImTryingToSaveYou");
+            harmony.Patch(
+                AccessTools.Method(typeof(Faction), nameof(Faction.Notify_MemberStripped)),
+                prefix: new HarmonyMethod(typeof(Patch_Faction_Notify_MemberStripped), nameof(Notify_MemberStripped_Prefix)),
+                postfix: new HarmonyMethod(typeof(Patch_Faction_Notify_MemberStripped), nameof(Notify_MemberStripped_Postfix))
+            );
+
+            harmony.Patch(
+                AccessTools.Method(typeof(JobDriver_ForceTargetWear), "MakeNewToils"),
+                postfix: new HarmonyMethod(typeof(Patch_Faction_Notify_MemberStripped), nameof(MakeNewToils_Postfix))
+            );
+            // Log.Message("[ImTryingToSaveYou] Loader loaded, all patches applied!");
+        }
+
+        // Called when MakeNewToils for ForceTargetWear completes; mark pawn as "just forcibly dressed"
+        public static void MakeNewToils_Postfix(JobDriver_ForceTargetWear __instance)
+        {
+            if (__instance?.job != null)
+            {
+                Pawn targetPawn = __instance.job.GetTarget(TargetIndex.A).Thing as Pawn;
+                if (targetPawn != null)
+                {
+                    JustForceDressed.Add(targetPawn);
+                   // Log.Message($"[ImTryingToSaveYou] Marking {targetPawn} as just forcibly dressed.");
+                }
+            }
+        }
+
+        // Prefix for Notify_MemberStripped - skip penalty if we just forcibly dressed this pawn
+        public static bool Notify_MemberStripped_Prefix(Pawn member, Faction violator)
+        {
+            if (JustForceDressed.Contains(member))
+            {
+               // Log.Message($"[ImTryingToSaveYou] Suppressing penalty for {member} stripped by {violator}.");
+                JustForceDressed.Remove(member);
+                return false; // skip original, do NOT apply penalty
+            }
+            return true; // run vanilla
+        }
+
+        // Postfix to clean up state (for any edge case)
+        public static void Notify_MemberStripped_Postfix(Pawn member, Faction violator)
+        {
+            JustForceDressed.Remove(member);
+        }
+    }
+}
---
+++ RW.ImTryingToSaveYou/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("RW.ImTryingToSaveYou")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RW.ImTryingToSaveYou")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2909f208-bbf2-431e-b484-95b47b49a259")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
